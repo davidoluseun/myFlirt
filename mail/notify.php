79a6e52d4a2c539a47ea692a1c437124db1868df
David Oluseun <a.davidoluseun@gmail.com>
2020-07-14 13:55:17 +0100

    Add php file

---
+++ mail/notify.php
@@ -0,0 +1,18 @@
+<?php
+        
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "db_myflirt";
+
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+    $firstname = $_POST["firstname"];
+    $email = $_POST["user-email"];
+
+    $sql = "INSERT INTO users (user_first, user_email) VALUES('$firstname', '$email');";
+    mysqli_query($conn, $sql);
+
+    header("Location: ../index.html?=success");
+
+?>
