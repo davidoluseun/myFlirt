ec135678c8bada1187c51f1c1f4e4467c262a101
David Oluseun <a.davidoluseun@gmail.com>
2020-07-17 10:23:37 +0100

    Add pop-up to screens images

--- js/script.js
+++ js/script.js
@@ -6,8 +6,56 @@
     $(function() {
 
         // Wow.js activation
+
         new WOW().init();
 
+        // Colorbox activation
+
+        $(".screen-links").colorbox({
+            "rel" : "gallery",
+            "maxWidth" : "80%",
+            "maxHeight" : "90%",
+            "transition" : "elastic",
+            "speed" : 700
+        });
+
+        // Use smooth scrolling when clicking on cta
+
+        var topoffset = 20; // Variable for menu height
+
+        $(".hero-cta").click(function() {
+            if (location.pathname.replace(/^\//,"") ===
+                this.pathname.replace(/^\//,"") &&
+                location.hostname === this.hostname) {
+
+                    var target = $(this.hash);
+                    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+
+                    if (target.length) {
+                        $("html, body").animate({
+                            scrollTop: target.offset().top + topoffset
+                        }, 500);
+                        return false;
+
+                    } //target.length
+            } 
+
+        }); //smooth scrolling
+ 
+        // Bootstrap form validation
+        // Fetch the form to apply custom Bootstrap validation style to
+        var forms = document.getElementsByClassName("notify-me-form");
+
+        // Loop over it and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener("submit", function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add("was-validated");
+            }, false);
+        });
   
     }); // jQuery document ready function
 
--- mail/notify.php
+++ mail/notify.php
@@ -1,18 +1,35 @@
 <?php
-        
+    
+    // Database detail
     $dbServername = "localhost";
     $dbUsername = "root";
     $dbPassword = "";
     $dbName = "db_myflirt";
 
+    // Create a connection to the database
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-    $firstname = $_POST["firstname"];
-    $email = $_POST["user-email"];
+    // Get users input
+    $firstname = mysqli_real_escape_string($conn, $_POST["firstname"]);
+    $email = mysqli_real_escape_string($conn, $_POST["user-email"]);
 
-    $sql = "INSERT INTO users (user_first, user_email) VALUES('$firstname', '$email');";
-    mysqli_query($conn, $sql);
+    // Get users email address from the database if it already exist
+    $get_user = "SELECT * FROM users WHERE user_email='$email';";
+    $result = mysqli_query($conn, $get_user);
 
-    header("Location: ../index.php?signup=success");
+    $resultCheck = mysqli_num_rows($result);
+
+    // Check if users email address already exist in the database
+    if ($resultCheck > 0) {
+
+        echo "Your email address already exist in our database";
+
+    } else {
+
+        $sql = "INSERT INTO users (user_first, user_email) VALUES('$firstname', '$email');";
+        mysqli_query($conn, $sql);
+    
+        header("Location: ../index.html?signup=success");
+    }
 
 ?>
