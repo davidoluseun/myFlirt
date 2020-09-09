ca204b3845db84285978852a566336349cfd7b06
David Oluseun <a.davidoluseun@gmail.com>
2020-09-09 14:52:59 +0100

    Update PHP script to reflect feedback

--- mail/notify.php
+++ mail/notify.php
@@ -1,17 +1,41 @@
 <?php
     
+    // Get user's inputs
+    $firstname = $_POST["firstname"];
+    $email = $_POST["useremail"];
+
+    // If inputs are empty
+    if (empty($firstname) || empty($email)) {
+
+        // output "required" and terminate the script
+        exit("required");
+    }
+
+    // Sanitize inputs
+    $nameRegex = "/^[a-zA-Z ]*$/";
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    // Validate user's inputs
+    // If inputs are invalid 
+    if (!preg_match($nameRegex, $firstname) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        
+        // output "invalid" and terminate the script
+        exit("invalid");
+    
+    }
+
     // Database detail
     $dbServername = "localhost";
-    $dbUsername = "myflirta0d";
-    $dbPassword = "Gd0bD*n+RRGKw?WA";
-    $dbName = "db_myflirt";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "db_feedback";
 
     // Create a connection to the database
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-    // Get users input
-    $firstname = mysqli_real_escape_string($conn, $_POST["firstname"]);
-    $email = mysqli_real_escape_string($conn, $_POST["user-email"]);
+    // Escape inputs
+    $firstname = mysqli_real_escape_string($conn, $firstname);
+    $email = mysqli_real_escape_string($conn, $email);
 
     // Get users email address from the database if it already exist
     $get_user = "SELECT * FROM users WHERE user_email='$email';";
@@ -20,16 +44,20 @@
     $resultCheck = mysqli_num_rows($result);
 
     // Check if users email address already exist in the database
+    // If it exist 
     if ($resultCheck > 0) {
 
-        echo "Your email address already exist in our database";
+        // output "exist" and terminate the script
+        exit("exist");
 
     } else {
 
+        // If it doesn't, insert the user's email in the database, output "saved" and terminate the script
         $sql = "INSERT INTO users (user_first, user_email) VALUES('$firstname', '$email');";
         mysqli_query($conn, $sql);
-    
-        header("Location: ../index.html?signup=success");
+
+        exit("saved");
+
     }
 
 ?>
